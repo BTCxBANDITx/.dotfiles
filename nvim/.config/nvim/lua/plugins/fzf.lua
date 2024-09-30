267ed47acff5e20932ab08ad0218baa4952c23c5
HACK3R_B4NDIT <141981595+BTCxBANDITx@users.noreply.github.com>
2024-09-30 06:58:00 -0700

    Add files via upload

---
+++ nvim/.config/nvim/lua/plugins/fzf.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					fzf = {
+						fuzzy = true,
+						override_generic_sorter = true,
+						override_file_sorter = true,
+						case_mode = "smart_case",
+					},
+				},
+			})
+			require("telescope").load_extension("fzf")
+		end,
+	},
+}
