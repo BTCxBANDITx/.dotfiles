d019b5632af94f51c1f9a44effc7461a511f7a85
HACK3R_B4NDIT <141981595+BTCxBANDITx@users.noreply.github.com>
2024-09-30 06:56:51 -0700

    Add files via upload

---
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,18 @@
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.relativenumber = true
+vim.g.mapleader = " "
+
+vim.opt.swapfile = false
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.wo.number = true
