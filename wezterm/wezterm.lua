a32a16501c20fa68de44225699613128de401737
HACK3R_B4NDIT <141981595+BTCxBANDITx@users.noreply.github.com>
2025-04-24 20:36:51 -0700

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,16 +4,17 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.color_scheme = "Neon"
+config.color_scheme = 'Laser'
 
 config.font = wezterm.font("Xanh Mono")
 config.font_size = 19
-config.cell_width = 0.6
-
+config.cell_width = 1.0
 
 config.enable_tab_bar = false
 
-config.window_background_opacity = 0.5
-config.text_background_opacity = 0.5
+config.window_background_opacity = 0.25
+config.text_background_opacity = 0.50
 
+bold_brightens_ansi_colors = true
+config.default_cursor_style = 'BlinkingBlock'
 return config
